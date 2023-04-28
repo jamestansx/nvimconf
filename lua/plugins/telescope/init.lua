a4ae94b955e7776a9df13b26f52e81d0e58433ca
James Tan <jamestansx@gmail.com>
2023-04-28 17:17:54 +0800

    ui select

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,15 +1,23 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  version = "0.1.x",
-  cmd = "Telescope",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    { "nvim-telescope/telescope-ui-select.nvim", event = "UIEnter" },
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.x",
+    cmd = "Telescope",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
+    end,
   },
-  config = function(_, opts)
-    require("telescope").setup(opts)
-    require("telescope").load_extension("fzf")
-    require("telescope").load_extension("ui-select")
-  end,
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    event = "UIEnter",
+    dependencies = "nvim-telescope/telescope.nvim",
+    config = function()
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
