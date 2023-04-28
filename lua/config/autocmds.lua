457386eec3c137a1e60775190979d3d829b6e50e
James Tan <jamestansx@gmail.com>
2023-04-28 16:23:59 +0800

    autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -30,3 +30,109 @@ autocmd({ "BufReadPost" }, {
     end
   end,
 })
+
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  desc = "Check if buffer were changed outside of neovim",
+  group = augroup("CheckTime"),
+  pattern = "*",
+  command = "checktime",
+})
+
+local togglecursorline = augroup("ToggleCursorLine")
+autocmd({ "InsertLeave", "WinEnter", "BufEnter" }, {
+  desc = "Auto show cursorline",
+  group = togglecursorline,
+  pattern = "*",
+  callback = function(ev)
+    if ev.event == "InsertLeave" then
+      vim.opt_local.cursorlineopt = "both"
+    end
+    if not vim.opt_local.cursorline:get() then
+      vim.opt_local.cursorline = true
+    end
+  end,
+})
+
+autocmd({ "InsertEnter", "WinLeave", "BufLeave" }, {
+  desc = "Auto hide cursorline",
+  group = togglecursorline,
+  pattern = "*",
+  callback = function(ev)
+    if ev.event == "InsertEnter" then
+      vim.opt_local.cursorlineopt = "number"
+      return
+    end
+    if vim.opt_local.cursorline:get() then
+      vim.opt_local.cursorline = false
+    end
+  end,
+})
+
+autocmd({ "BufWritePre" }, {
+  desc = "Create directories if needed when saving a file",
+  group = augroup("MkdirOnSave"),
+  pattern = "*",
+  callback = function(ev)
+    local file = uv.fs_realpath(ev.match) or ev.match
+    fn.mkdir(fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+autocmd({ "VimEnter" }, {
+  desc = "Set cwd when opening Neovim",
+  group = augroup("AutoCwd"),
+  pattern = "*",
+  callback = function(ev)
+    local dir = fn.fnamemodify(ev.file, ":p:h")
+    api.nvim_set_current_dir(dir:match("^drex://(.*)$") or dir)
+  end
+})
+
+autocmd({ "BufRead", "BufNewFile" }, {
+  desc = "Prevent accidental write to buffers that shouldn't be editted",
+  group = augroup("NoWrite"),
+  pattern = {
+    "*.orig",
+    "*.pacnew",
+  },
+  command = [[setlocal nomodifiable]],
+})
+
+autocmd({ "BufWritePre" }, {
+  desc = "Don't save undo on certain files",
+  group = augroup("NoUndo"),
+  pattern = {
+    "/tmp/*",
+    "COMMIT_EDITMSG",
+    "MERGE_MSG",
+    "*.tmp",
+    "*.bak",
+  },
+  command = [[setlocal noundofile]],
+})
+
+autocmd({ "FileType" }, {
+  desc = "Close filetypes with <q>",
+  group = augroup("KeymapQuit"),
+  pattern = {
+    "tsplayground",
+    "checkhealth",
+    "startuptime",
+    "lspinfo",
+    "notify",
+    "help",
+    "man",
+    "qf",
+  },
+  callback = function(ev)
+    vim.bo[ev.buf].buflisted = false
+    util.keymap("n", "q", "<Cmd>close<CR>", { buffer = ev.buf, silent = true })
+  end,
+})
+
+local setft = augroup("SetFt")
+autocmd({ "BufRead", "BufNewFile" }, {
+  group = setft,
+  pattern = "*.env*",
+  command = [[setfiletype env]],
+})
